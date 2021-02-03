9b5c12faa3de0f4b8dd291d33541c591da9f3df6
rhnptl79 <rhnptl79@gmail.com>
2021-02-03 15:39:47 +0530

    NoteList 6

--- note_CYBAGE_iOS/NoteListViewController.swift
+++ note_CYBAGE_iOS/NoteListViewController.swift
@@ -64,35 +64,43 @@ class NoteListViewController: UITableViewController {
         return notes.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "note_cell", for: indexPath)
+        
+        let note = notes[indexPath.row]
+        cell.textLabel?.text = note.title
+        
+        let backgroundView = UIView()
+//        backgroundView.backgroundColor = .darkGray
+        cell.selectedBackgroundView = backgroundView
+        
         return cell
     }
-    */
+    
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            let note = notes[indexPath.row]
+            self.deleteNote(note: note)
+            notes.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        }
+        else if editingStyle == .insert {
+        }
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
