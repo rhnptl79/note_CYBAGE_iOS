706998af1da4ac537f719f50fb933816585c8539
rhnptl79 <rhnptl79@gmail.com>
2021-02-03 14:48:41 +0530

    Folder 6

--- note_CYBAGE_iOS/folderListViewController.swift
+++ note_CYBAGE_iOS/folderListViewController.swift
@@ -146,14 +146,19 @@ class folderListViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        let destination = segue.destination as! NoteListViewController
+        if let indexPath = tableView.indexPathForSelectedRow{
+            destination.selectedFolder = folders[indexPath.row]
+        }
+
     }
-    */
+    
 
 }
